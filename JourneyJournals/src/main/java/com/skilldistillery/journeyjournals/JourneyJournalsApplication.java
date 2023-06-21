482acc5e3bdb384f37b19554c1f002bfb0673201
pasaye <pasayejoseph@yahoo.com>
2023-06-21 11:50:41 -0500

    spring boot started

---
+++ JourneyJournals/src/main/java/com/skilldistillery/journeyjournals/JourneyJournalsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.journeyjournals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyJournalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyJournalsApplication.class, args);
+	}
+
+}
---
+++ JourneyJournals/src/test/java/com/skilldistillery/journeyjournals/JourneyJournalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.journeyjournals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyJournalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
