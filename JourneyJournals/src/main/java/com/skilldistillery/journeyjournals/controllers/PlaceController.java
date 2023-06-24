1bc0e8f8fabbcf9a437c6b27d3d22ee7f3d43339
Jacob <jwv928@gmail.com>
2023-06-24 16:26:15 -0400

    added create Places

--- JourneyJournals/src/main/java/com/skilldistillery/journeyjournals/controllers/PlaceController.java
+++ JourneyJournals/src/main/java/com/skilldistillery/journeyjournals/controllers/PlaceController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,6 +49,20 @@ public class PlaceController {
 			return null;
 		}
 		return places;
+	}
+	
+	@PostMapping("places")
+	private Place createPlace(@RequestBody Place place, Principal principal, HttpServletResponse res) {
+		
+		try {
+			place = placeServ.create(principal.getName(), place);
+			res.setStatus(201);
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(404);
+			return null;
+		}
+		return place;
 	}
 
 }
