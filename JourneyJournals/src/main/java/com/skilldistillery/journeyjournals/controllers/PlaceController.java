04b37dc1202f9f6d917dc73dffdde882b1ce5792
Jacob <jwv928@gmail.com>
2023-06-24 13:02:52 -0400

    search by place Id works on backend

---
+++ JourneyJournals/src/main/java/com/skilldistillery/journeyjournals/controllers/PlaceController.java
@@ -0,0 +1,35 @@
+package com.skilldistillery.journeyjournals.controllers;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.journeyjournals.entities.Place;
+import com.skilldistillery.journeyjournals.services.PlaceService;
+
+@RestController
+@RequestMapping("api")
+@CrossOrigin({"*", "http://localhost/"})
+public class PlaceController {
+	
+	@Autowired
+	private PlaceService placeServ;
+	
+	@GetMapping("places/{id}")
+	private Place findPlaceById(@PathVariable int id, Principal principal, HttpServletResponse res) {
+		Place place = placeServ.show(principal.getName(), id);
+		if (place == null) {
+			res.setStatus(404);
+		}
+		
+		return place;
+	}
+
+}
