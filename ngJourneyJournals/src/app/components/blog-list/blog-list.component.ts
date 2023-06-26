414357740c6e9d203cf93a4af2140ea6a4d4d9e7
pasaye <pasayejoseph@yahoo.com>
2023-06-26 17:16:52 -0500

    blog details and table working

--- ngJourneyJournals/src/app/components/blog-list/blog-list.component.ts
+++ ngJourneyJournals/src/app/components/blog-list/blog-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Blog } from 'src/app/models/blog';
 import { BlogRating } from 'src/app/models/blog-rating';
 import { User } from 'src/app/models/user';
@@ -9,25 +9,36 @@ import { BlogServiceService } from 'src/app/services/blog-service.service';
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.css']
 })
-export class BlogListComponent {
+export class BlogListComponent implements OnInit {
 
   blogs: Blog[] = [];
   user: User | null = null;
-
+  selected: Blog | null = null;
 
   constructor(private blogServ: BlogServiceService,
   ) { }
 
   ngOnInit(): void {
+    this.reload();
+  }
+
+  reload() {
     this.blogServ.indexAll().subscribe({
-      next: (blogList) => {
-        this.blogs = blogList;
-        console.log(this.blogs[0])
+      next: (bloglist) => {
+        this.blogs = bloglist;
       },
       error: (err) => {
         console.error(err);
-      },
+      }
     })
+  }
+
+  displayBlog(blog: Blog): void {
+    this.selected = blog;
+  }
+
+  displayTable(): void {
+    this.selected = null;
   }
 
 }
