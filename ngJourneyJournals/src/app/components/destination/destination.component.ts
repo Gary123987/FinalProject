dfdd2bb5d8151ea287affc090c9fe302ffd04997
Jacob <jwv928@gmail.com>
2023-06-27 12:31:26 -0400

    added css to destination page

--- ngJourneyJournals/src/app/components/destination/destination.component.ts
+++ ngJourneyJournals/src/app/components/destination/destination.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Country } from 'src/app/models/country';
 import { Continent } from 'src/app/models/continent';
+import { Destination } from 'src/app/models/destination';
+import { DestinationService } from 'src/app/services/destination.service';
 
 @Component({
   selector: 'app-destination',
@@ -14,16 +16,19 @@ export class DestinationComponent implements OnInit{
 
   countries: Country[] = [];
   continents: Continent[] = [];
+  destinations: Destination [] = [];
 
   constructor(
   private continentServ: ContinentService,
   private countryServ: CountryService,
   private auth: AuthService,
+  private destinationServ: DestinationService,
   ){}
 
   ngOnInit(){
   this.loadContinents();
   this.loadCountries();
+  this.loadDestinations();
   }
 
   checkedLogin(){
@@ -46,6 +51,18 @@ export class DestinationComponent implements OnInit{
     this.countryServ.indexAll().subscribe({
       next: (countryList) => {
         this.countries = countryList;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+
+  }
+
+  loadDestinations(){
+    this.destinationServ.indexByUser().subscribe({
+      next: (destinationList) => {
+        this.destinations = destinationList;
       },
       error: (err) => {
         console.log(err);
