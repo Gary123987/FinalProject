c0bbc9600e32cbcc929df7d4ae32f367b66f6dff
pasaye <pasayejoseph@yahoo.com>
2023-06-29 07:14:52 -0500

    destination list complete

--- ngJourneyJournals/src/app/components/destination-list/destination-list.component.ts
+++ ngJourneyJournals/src/app/components/destination-list/destination-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Destination } from 'src/app/models/destination';
+import { User } from 'src/app/models/user';
+import { DestinationService } from 'src/app/services/destination.service';
 
 @Component({
   selector: 'app-destination-list',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./destination-list.component.css']
 })
 export class DestinationListComponent {
+  destinations: Destination[] = [];
+  user: User | null = null;
+  selected: Destination | null = null;
+
+  constructor(private desServ: DestinationService,
+  ) { }
+
+  ngOnInit(): void {
+    this.reload();
+  }
+
+  reload() {
+    this.desServ.indexByUser().subscribe({
+      next: (deslist) => {
+        this.destinations = deslist;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  displayBlog(des: Destination): void {
+    this.selected = des;
+  }
+
+  displayTable(): void {
+    this.selected = null;
+  }
 
 }
